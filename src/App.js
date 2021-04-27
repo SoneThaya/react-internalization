c2b5b7aafcca5020896662809ca5ffeaefe5a80a
sone <sthaya@gmail.com>
2021-04-26 21:55:27 -0700

    created language json files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FormattedMessage } from "react-intl";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <FormattedMessage
+          id="app.text"
+          defaultMessage="Edit <code>src/App.js</code> and save to reload.<br/>Now with {what}!"
+          description="Welcome header on app main page"
+          values={{ what: "react-intl" }}
+        />
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FormattedMessage
+          id="app.learn-react-link"
+          defaultMessage="Learn React"
+          description="Link on react page"
+        />
+      </a>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { IntlProvider } from "react-intl";
+
+import messages_de from "./translations/de.json";
+import messages_en from "./translations/en.json";
+
+const messages = {
+    'de': messages_de,
+    'en': messages_en
+};
+const language = navigator.language.split(/[-_]/)[0];
 
 ReactDOM.render(
-  <React.StrictMode>
+  <IntlProvider locale={language} messages={messages[language]}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </IntlProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
